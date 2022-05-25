086f3e0f9fcd950e002688e65325ceedfa223468
Paulo <pc5783346@gmail.com>
2022-05-24 21:21:59 -0300

    doc: installing the necessary libs and creating the basic components

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+
+import "./components/assets/css/reset.css";
+import "./components/assets/css/style.css";
+
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.querySelector(".root"));
